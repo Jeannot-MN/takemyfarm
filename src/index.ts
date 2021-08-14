d147c636300b743219fbebe327e20eefa2f79a40
JeannotMN <jeannot@ssquared.dev>
2021-08-14 19:12:35 +0200

    Database configuration done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello world");
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import dataSourceConfiguration from './db/config/DatasourceConfiguration'
+
+// const SERVER_PORT = process.env.SERVER_PORT || 8080;
+
+createConnection(dataSourceConfiguration)
+    //   .then((_connection) => {
+    //     app.listen(PORT, () => {
+    //       console.log("Server is running on port", PORT);
+    //     });
+    //   })
+    .then((_connection) => {
+        console.log("Connected to the DB");
+
+    })
+    .catch((err) => {
+        console.log("Unable to connect to db", err);
+        process.exit(1);
+    });
