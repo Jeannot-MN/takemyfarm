98b7f6b184681a6a819f7da2c717bc63c2282c48
JeannotMN <jeannot@ssquared.dev>
2021-08-22 12:02:38 +0200

    init client

--- server/src/graphql/resolvers/user/UserQueryResolver.ts
+++ server/src/graphql/resolvers/user/UserQueryResolver.ts
@@ -1,20 +1,29 @@
-import { Query, Resolver } from "type-graphql";
+import { Ctx, Query, Resolver } from "type-graphql";
 import { Container } from "typeorm-typedi-extensions";
 import { UserService } from "../../../service/user/UserService";
+import { GraphqlContext } from "../../context/GraphqlContext";
+import { UserMapper } from "../../mappers/UserMapper";
 import { UserDTO } from "../../types/user/UserDTO";
 
 @Resolver()
 export class UserQueryResolver {
 
     private userService: UserService;
+    private userMapper: UserMapper;
 
     constructor(){
-        this.userService = Container.get(UserService)
+        this.userService = Container.get(UserService);
+        this.userMapper = Container.get(UserMapper);
     }
 
     @Query(() => [UserDTO])
     async users() {
         const users = await this.userService.findAll();
         return users;
+    }
+
+    @Query(() => UserDTO)
+    async me(@Ctx() context: GraphqlContext){
+        return context.user;
     }
 }
